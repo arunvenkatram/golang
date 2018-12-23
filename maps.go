37a6d5db4e64bc24a6b2a36ff0562aa3f935b84b
arun venkatram <arun92venkat@gmail.com>
2018-12-24 00:38:27 +0530

    new file:   maps.go

---
+++ maps.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arunmap := make(map[string]int)
+	arunmap["go"] = 2
+	arunmap["python"] = 5
+	venkatmap := map[string]int{
+		"docker":     6,
+		"Kubernetes": 9,
+	}
+	fmt.Printf("Arun is %v\nVenkat is %v", arunmap, venkatmap)
+}
